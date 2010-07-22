6a9cd2bafd2dc64c0b2e635fa90be9f97e6ae223
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2010-07-22 17:31:42 -0500

    Adding the initial unit test.

---
+++ test/run_creole_tests.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+#
+# Imports
+#
+
+# System Imports
+import unittest
+
+#
+# Unit Test
+#
+
+class CreoleDocbookTests(unittest.TestCase):
+    def test_one_h1(self):
+        print "bob"
+
+#
+# Entry
+#
+
+if __name__ == '__main__':
+    unittest.main()
