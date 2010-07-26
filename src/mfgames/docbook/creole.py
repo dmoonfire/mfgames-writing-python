217b7e2f709bd22a27032c58d3b8e449073ec5d1
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2010-07-25 20:09:26 -0500

    Taught mfgames-docbook how to export Creole output for round-tripping.

--- src/mfgames/docbook/creole.py
+++ src/mfgames/docbook/creole.py
@@ -5,11 +5,74 @@
 # System Imports
 import argparse
 import logging
+import os
+import textwrap
+import xml.sax
 
 # Internal Imports
 import mfgames.constants
 import mfgames.convert
 import mfgames.process
+
+#
+# SAX Document Handler
+#
+
+class DocbookCreoleHandler(xml.sax.ContentHandler):
+    def __init__(self, args, output):
+        self.args = args
+        self.output = output
+        self.buffer = ""
+        self.depth = 0
+
+        if args.columns > 0:
+            self.wrapper = textwrap.TextWrapper()
+            self.wrapper.width = args.columns
+
+    def characters(self, contents):
+        self.buffer += contents
+
+    def startElement(self, name, attrs):
+        # Based on the name of the attribute determines what we do
+        # with the character contents.
+        if name == "article":
+            self.depth = 1
+
+        if name == "section":
+            self.depth = self.depth + 1
+            self.output.write(os.linesep)
+
+        if name == "simpara":
+            self.output.write(os.linesep)
+
+    def endElement(self, name):
+        # Based on the attribute name determines how we close the elements.
+        if name == "section":
+            self.depth = self.depth - 1
+
+        if name == "title":
+            # Write out the section prefix
+            self.output.write("=" * self.depth)
+            self.output.write(' ')
+            
+            # Write out the title itself.
+            self.output.write(self.wrap_buffer())
+            self.output.write(os.linesep)
+
+            # Clear the buffer for the next paragraph.
+            self.buffer = ""
+
+        if name == "simpara":
+            self.output.write(self.wrap_buffer())
+            self.output.write(os.linesep)
+            self.buffer = ""
+
+    def wrap_buffer(self):
+        if self.args.columns > 0:
+            results = self.wrapper.fill(self.buffer)
+            return results
+
+        return self.buffer
 
 #
 # Conversion Class
@@ -26,11 +89,30 @@ class DocbookCreoleConvertProcess(mfgames.convert.ConvertProcess):
         """
         Converts the given file into Creole.
         """
-    
+
+        # Open the output stream.
+        output = open(output_filename, 'w')
+
+        # Open an SAX XML stream for the DocBook contents.
+        parser = xml.sax.make_parser()
+        parser.setContentHandler(DocbookCreoleHandler(args, output))
+        parser.parse(open(input_filename))
+
+        # Close the output file.
+        output.close()
+
     def setup_arguments(self, parser):
         """
         Sets up the command-line arguments for the Docbook to Creole
         conversion.
         """
 
+        # Add in the argument from the base class.
         super(DocbookCreoleConvertProcess, self).setup_arguments(parser)
+
+        # Add in the Creole-specific generations.
+        parser.add_argument(
+            '--columns',
+            default=0,
+            type=int,
+            help="Sets the number of columns to wrap outout.")
--- test/run_docbook_tests.py
+++ test/run_docbook_tests.py
@@ -18,7 +18,7 @@ src_directory = os.path.realpath(local_directory + "/../src")
 sys.path.append(src_directory)
 
 # Internal Import
-src_module = os.path.join(src_directory, 'mfgames-creole.py')
+src_module = os.path.join(src_directory, 'mfgames-docbook.py')
 
 tool_code = open(src_module, 'rb')
 tool = imp.load_source(
@@ -30,13 +30,13 @@ tool = imp.load_source(
 # Unit Test
 #
 
-class CreoleDocbookTests(unittest.TestCase):
+class DocbookCreoleTests(unittest.TestCase):
     def run_tool(self, name, parameters):
         # Figure out the filenames of the input files.
         base_filename = os.path.join(local_directory, name)
-        input_filename = base_filename + ".txt"
+        input_filename = base_filename + ".xml"
         expected_filename = name + ".expected"
-        results_filename = name + ".xml"
+        results_filename = name + ".txt"
         log_filename = name + ".log"
 
         # Add parameters to make the output as silent as possible.
@@ -52,7 +52,7 @@ class CreoleDocbookTests(unittest.TestCase):
 
         # Run the tool with the given parameters.
         #print(parameters)
-        tool.do_creole_tool(parameters)
+        tool.do_docbook_tool(parameters)
 
         # Load in the expected and resulting file.
         expected = open(expected_filename, 'rb')
@@ -75,30 +75,20 @@ class CreoleDocbookTests(unittest.TestCase):
         if os.path.exists(log_filename):
             os.remove(log_filename)
 
-    def test_localwords(self):
-        self.run_tool(
-            'creole/creole_docbook_localwords',
-            ['docbook', '--ignore-localwords'])
-
     def test_one_h1(self):
         self.run_tool(
-            'creole/creole_docbook_one_h1',
-            ['docbook'])
-
-    def test_two_h1(self):
-        self.run_tool(
-            'creole/creole_docbook_two_h1',
-            ['docbook'])
+            'docbook/docbook_creole_one_h1',
+            ['creole'])
 
     def test_two_h2(self):
         self.run_tool(
-            'creole/creole_docbook_two_h2',
-            ['docbook'])
+            'docbook/docbook_creole_two_h2',
+            ['creole'])
 
     def test_long_para(self):
         self.run_tool(
-            'creole/creole_docbook_long_para',
-            ['docbook'])
+            'docbook/docbook_creole_long_para',
+            ['creole', '--columns', '70'])
 
 #
 # Entry
