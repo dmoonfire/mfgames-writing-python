9a85a7e1f8425699e6b70377f70cd66c94f7942a
Dylan R. E. Moonfire <d.moonfire@mfgames.com>
2012-04-12 22:55:30 -0500

    Modified the setup.py to install `mfgames-ncx` and `mfgames-opf` along with all the associated modules. Also bumped the version up to 0.1.0.0 since new functionality was included.

--- setup.py
+++ setup.py
@@ -13,13 +13,15 @@ from distutils.core import setup
 
 setup(
     name='mfgames-writing',
-    version='0.0.1.0',
+    version='0.1.0.0',
     description='Utilities for text-base writing.',
-    author='D. Moonfire',
+    author='Dylan R. E. Moonfire',
     url='http://mfgames.com/mfgames-writing',
     scripts=[
         'src/mfgames-creole',
         'src/mfgames-docbook',
+        'src/mfgames-ncx',
+        'src/mfgames-opf',
         ],
     data_files=[
         ('share/mfgames-writing', [
@@ -32,6 +34,7 @@ setup(
             ]),
         ('share/mfgames-writing/writing', [
             'src/writing/__init__.py',
+            'src/writing/constants.py',
             'src/writing/creole.py',
             'src/writing/format.py',
             ]),
@@ -41,6 +44,23 @@ setup(
             'src/writing/docbook/info.py',
             'src/writing/docbook/scan.py',
             'src/writing/docbook/text.py',
+            ]),
+        ('share/mfgames-writing/writing/ncx', [
+            'src/writing/ncx/__init__.py',
+            'src/writing/ncx/meta.py',
+            'src/writing/ncx/author.py',
+            'src/writing/ncx/title.py',
+            'src/writing/ncx/nav.py',
+            ]),
+        ('share/mfgames-writing/writing/opf', [
+            'src/writing/opf/__init__.py',
+            'src/writing/opf/dc.py',
+            'src/writing/opf/cover.py',
+            'src/writing/opf/spine.py',
+            'src/writing/opf/manifest.py',
+            'src/writing/opf/uid.py',
+            'src/writing/opf/toc.py',
+            'src/writing/opf/guide.py',
             ]),
         ],
     )
